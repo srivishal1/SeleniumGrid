8d565cb9b7cbe0e4d3565f8adb3c2ea1a19653d7
srivishal1 <vishal.srivastava@3pillarglobal.com>
2025-07-06 00:17:44 +0530

    Update homePage.java

--- src/main/java/pagefunctions/homePage.java
+++ src/main/java/pagefunctions/homePage.java
@@ -9,9 +9,9 @@ public class homePage extends homePage_OR {
 	public homePage (WebDriver driver) throws Exception {
 		super(driver);
 		this.driver = driver;
-		if(!driverFunctionLib.isElementPresent(contactUs)) {
-			throw new IllegalStateException("This is not home Page");
-		}
+		// if(!driverFunctionLib.isElementPresent(contactUs)) {
+		// 	throw new IllegalStateException("This is not home Page");
+		// }
 	}
 
 	public void getCurrentTime() {
